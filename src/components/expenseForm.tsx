e53928e2c56b05c7054ef91cbea03485bc3c87c9
israelLuna13 <c24.e.israel.luna@gmail.com>
2024-05-14 09:11:15 -0600

    Se agrego el formulario y el calendario para la fecha de un gasto

--- src/components/expenseForm.tsx
+++ src/components/expenseForm.tsx
@@ -1,7 +1,89 @@
-import React from 'react'
+import { categories } from "../data/categories";
+
+import DatePicker from 'react-date-picker';
+import 'react-date-picker/dist/DatePicker.css';
+import 'react-calendar/dist/Calendar.css';
+type ValuePiece = Date | null;
+
+type Value = ValuePiece | [ValuePiece, ValuePiece];
 
 export default function expenseForm() {
+  
   return (
-    <div>expenseForm</div>
-  )
+    <form action="" className="space-y-5">
+      <legend
+        className="uppercase text-center text-2xl font-black border-b-4
+    border-blue-500 py-2"
+      >
+        Nuevo Gasto
+      </legend>
+
+      <div className="flex flex-col gap-2">
+        <label htmlFor="expenseName" className="text-xl">
+          Nombre del gasto:
+        </label>
+
+        <input
+          type="text"
+          id="expenseName"
+          placeholder="Añanade el nombre del gasto"
+          className="bg-slate-100 p-2"
+          name="expenseName"
+        />
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <label htmlFor="amount" className="text-xl">
+          Cantidad:
+        </label>
+
+        <input
+          type="number"
+          id="amount"
+          placeholder="Añande la cantidad"
+          className="bg-slate-100 p-2"
+          name="amount"
+        />
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <label htmlFor="category" className="text-xl">
+          Categoria:
+        </label>
+
+        <select id="category" className="bg-slate-100 p-2" name="category">
+          <option value="">-- Seleccione --</option>
+          {categories.map((category) => (
+            <option value={category.id} key={category.id}>
+              {category.name}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <label htmlFor="amount" className="text-xl">
+          Fecga Gasto:
+        </label>
+        <DatePicker
+        className='bg-slate-100 p-2 border-0'
+        />
+
+        <input
+          type="number"
+          id="amount"
+          placeholder="Añande la cantidad"
+          className="bg-slate-100 p-2"
+          name="amount"
+        />
+      </div>
+
+      <input
+        type="submit"
+        className="bg-blue-600 cursor-pointer w-full p-2 text-white uppercase
+      font-bold rounded-lg"
+        value={"Registrar Gasto"}
+      />
+    </form>
+  );
 }
